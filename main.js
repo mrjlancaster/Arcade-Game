0057578c28bcdc7d779ee4bf9e9fb9547e988877
mrjlancaster <lancast001@gmail.com>
2020-05-12 22:05:42 -0500

    add moving ball functionality

--- main.js
+++ main.js
@@ -1,27 +1,40 @@
 let canvas;
 let canvasContext;
 let ballX = 50;
+let ballSpeedX = 10;
+
+
+
 window.onload = function() {
     console.log("Hello World!");
     canvas = document.getElementById('gameCanvas');
     canvasContext = canvas.getContext('2d');
 
     let framesPerSecond = 30;
-    setInterval(drawEverything, 1000/framesPerSecond);
+    setInterval(function() {
+        moveEverything();
+        drawEverything();
+     }, 1000/framesPerSecond);
 }
-
 
 function callBoth() {
     moveEverything();
     drawEverything();
 }
 
-
 function moveEverything() {
-    ballX = ballX + 5;
-
+    ballX = ballX + ballSpeedX;
+    if (ballX > canvas.width) {
+        ballSpeedX = -ballSpeedX;
+    } else if (ballX < 0) {
+        ballSpeedX = -ballSpeedX;
+    }
 }
 
+function colorRect(leftX, topY, width, height, drawColor) {
+    canvasContext.fillStyle = drawColor;
+    canvasContext.fillRect(leftX, topY, width, height);
+}
 
 function drawEverything() {
 
