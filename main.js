9da6333607d6f74bf2e1235e830052899dee1e10
mrjlancaster <lancast001@gmail.com>
2020-05-14 00:13:01 -0500

    add scores board for both players

--- main.js
+++ main.js
@@ -2,8 +2,11 @@ let canvas;
 let canvasContext;
 let ballX = 50;
 let ballY = 50;
-let ballSpeedX = 15;
+let ballSpeedX = 10;
 let ballSpeedY = 4;
+
+let player1Score = 0;
+let player2Score = 0;
 
 let paddle1Y = 250;
 let paddle2Y = 250;
@@ -44,19 +47,37 @@ function callBoth() {
     drawEverything();
 }
 
+function computerMovement() {
+    let paddle2YCenter = paddle2Y + (PADDLE_HEIGHT / 2);
+;    if (paddle2YCenter < ballY - 35) {
+        paddle2Y += 6;
+    } else if (paddle2YCenter > ballY + 35) {
+        paddle2Y -= 6;
+    }
+}
+
 function moveEverything() {
-    ballX = ballX + ballSpeedX;
-    ballY = ballY + ballSpeedY;
+    computerMovement();
+
+    ballX += ballSpeedX;
+    ballY += ballSpeedY;
     
     if (ballX < 0) {
         if (ballY > paddle1Y && ballY < paddle1Y + PADDLE_HEIGHT) {
             ballSpeedX = -ballSpeedX;
         } else {
             ballReset();
+            player2Score++;
         }
     }
     if (ballX > canvas.width) {
-        ballSpeedX = -ballSpeedX;
+        if (ballY > paddle2Y && ballY < paddle2Y + PADDLE_HEIGHT) {
+            ballSpeedX = -ballSpeedX;
+        } else {
+            ballReset();
+            player1Score++;
+
+        }
     }
     if (ballY < 0) {
         ballSpeedY = -ballSpeedY;
@@ -65,23 +86,6 @@ function moveEverything() {
         ballSpeedY = -ballSpeedY;
     }
 }
-
-
-
-//     //horizontal movement
-//     if (ballX > canvas.width) {
-//         //ballSpeedX = -ballSpeedX;
-//     } else if (ballX < 0) {
-//         ballSpeedX = -ballSpeedX;
-//     }
-
-//     //vertical movement
-//     if (ballY > canvas.height) {
-//         ballSpeedY = -ballSpeedY;
-//     } else if (ballY < 0) {
-//         ballSpeedY = -ballSpeedY;
-//     }
-// }
 
 function drawEverything() {
     colorRect(0, 0, canvas.width, canvas.height, 'black')
@@ -94,6 +98,9 @@ function drawEverything() {
 
     //next line draws the ball
     colorCircle(ballX, ballY, 10, 'white');
+
+    canvasContext.fillText(player1Score, 100, 100);
+    canvasContext.fillText(player2Score, canvas.width - 100, 100);
 }
 
 function colorCircle(centerX, centerY, radius, drawColor) {
